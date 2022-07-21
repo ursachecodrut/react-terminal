04a12c11f82e864a2ceb8feaa09fed4f0b7b42f6
Codrut Ursache <ursache.codrut71@gmail.com>
2022-07-21 15:18:43 +0300

    more styling. init history

--- src/Terminal/Terminal.tsx
+++ src/Terminal/Terminal.tsx
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HistoryItem } from '../models';
 import './terminal.css';
 
 const Terminal = () => {
+	const [history, setHistory] = useState<HistoryItem[]>([]);
+
+	const addToHistory = (newElem: HistoryItem) =>
+		setHistory((state) => [...state, newElem]);
+
+	const handleEnterKeyPress = (e: React.KeyboardEvent<HTMLElement>) => {
+		e.preventDefault();
+		// addToHistory({
+		// 	command: e.target.value,
+		// 	path: '.',
+		// 	output: '',
+		// });
+	};
+
 	return (
 		<main id="terminal">
 			<section id="terminal-header">
 				<div className="header-buttons">
-					<div className="circle red"></div>
-					<div className="circle orange"></div>
-					<div className="circle green"></div>
+					<div className="circle bg-red"></div>
+					<div className="circle bg-orange"></div>
+					<div className="circle bg-green"></div>
 				</div>
 			</section>
 			<section id="terminal-body">
-				<div></div>
+				{history.map((item, index) => (
+					<div key={index} className="history-item">
+						<div className="terminal-line">
+							<div className="host">
+								codrut@portfolio
+								<span className="text-foreground">:</span>
+							</div>
+							<div className="path">{item.path}</div>
+							<div className="text-foreground">$</div>
+							<div className="command">{item.command}</div>
+						</div>
+						<div className="output">{item.output}</div>
+					</div>
+				))}
+				<div className="terminal-line">
+					<div className="host">
+						codrut@portfolio
+						<span className="text-foreground">:</span>
+					</div>
+					<div className="path">~</div>
+					<div className="text-foreground">$</div>
+					<input
+						type="text"
+						id="terminal-input"
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+								handleEnterKeyPress(e);
+							}
+						}}
+						autoFocus
+					/>
+				</div>
 			</section>
 		</main>
 	);
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from './Item';
 export * from './File';
 export * from './Directory';
 export * from './FileSystem';
+export * from './HistoryItem';
